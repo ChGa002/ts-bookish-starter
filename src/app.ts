f94e6f68036e86dcd022848aab676d83cd16a8a7
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-14 16:08:26 +0000

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,48 @@ app.listen(port, () => {
  */
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
+
+const Connection = require('tedious').Connection;
+const config = {
+    server: 'CHAMELEON',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'cgandolfi',
+            password: 'Lollipop1',
+        },
+    },
+
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: true,
+        database: 'bookish',
+    },
+};
+const connection = new Connection(config);
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Connection Failed');
+        throw err;
+    }
+    // executeStatement();
+});
+
+function executeStatement() {
+    // @ts-ignore
+    const request = new Request('select * from Books', function (
+        err,
+        rowCount,
+    ) {
+        if (err) {
+            throw err;
+        }
+        console.log('DONE!');
+        connection.close();
+    });
+}
+
+app.get('/books', function (req, res) {
+    res.send('hello world TEST');
+});
---
+++ src/entities/Book.ts
@@ -0,0 +1,9 @@
+class Book {
+    isbn: number;
+    title: string;
+
+    constructor(isbn: number, title: string) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+}
