4c8e2bd97990e84e14e031dbd12de928dcfeafcb
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-15 17:01:50 +0000

    search route implemented only for title parameter

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import { Book } from '../models/Book';
 import { Connection } from 'tedious';
 import { Request as TediousRequest } from 'tedious';
+const { Op } = require("sequelize");
 
 // import {DatabaseConfig} from "../databaseConfig";
 
@@ -13,7 +14,7 @@ class BookController {
         this.router.get('/:id', this.getBook.bind(this));
         this.router.post('/create', this.createBook.bind(this));
         this.router.post('/:id/addcopy', this.addCopy.bind(this));
-        this.router.get('/getByTitle', this.getByTitle.bind(this));
+        this.router.post('/search', this.search.bind(this));
         this.router.get('/', this.getAllBooks.bind(this));
     }
 
@@ -41,12 +42,27 @@ class BookController {
         });
     }
 
-    getByTitle(req: Request, res: Response) {
-        // TODO: implement functionality
-        return res.status(500).json({
-            error: 'server_error',
-            error_description: 'Endpoint not implemented yet.',
-        });
+    search(req: Request, res: Response) {
+        if (req.query.title != undefined) {
+            Book.findAll({ where: { title: {[Op.like]:`%${req.query.title}%` }} })
+                .then((books) => {
+                    if (books.length > 0) {
+                        return res.status(200).json(books);
+                    }
+                    return res.status(204).json();
+                })
+                .catch((err) => {
+                    return res.status(500).json({
+                        error: 'Internal server error',
+                        error_description: 'Unable to get books: ' + err,
+                    });
+                });
+        } else {
+            return res.status(400).json({
+                error: 'Bad request',
+                error_description: 'Please specify a value for title',
+            });
+        }
     }
 
     getAllBooks(req: Request, res: Response) {
